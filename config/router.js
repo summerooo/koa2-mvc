3224cd43141854e5ef658db65d4af39ac1a098f8
WrenchDE <1695408430@qq.com>
2019-04-11 13:52:18 +0800

    optimization

--- config/router.js
+++ config/router.js
@@ -1,28 +1,24 @@
 const fs = require('fs')
-const path = require('path')
+const Path = require('path')
 const Router = require('koa-router')
 
 const router = Router()
-module.exports = (dir) => {
-    let relativePath = dir || path.resolve(__dirname, '..')
-    const files = fs.readdirSync(relativePath + '/api')
-    const js_files = files.filter( f => f.endsWith('.js'))
-    js_files.map(x => {
-      let mapping = require(relativePath + '/api/' + x)
-      if ('get' in mapping) {
-        for (let g in mapping.get) {
-          let path = '/' + x.substr(0, x.length - 3) + '/' + g
-          router.get(path, mapping.get[g])
+module.exports = dir => {
+  const relativePath = dir || Path.resolve(__dirname, '..')
+  const files = fs.readdirSync(relativePath + '/api')
+  const jsFiles = files.filter( f => f.endsWith('.js'))
+  jsFiles.map(file => {
+    let mapping = require(relativePath + '/api/' + file)
+    for (let way in mapping) {
+      if (way !== 'config') {
+        for (let method in mapping[way]) {
+          let path = '/' + file.substr(0, file.length - 3) + '/' + method
+          if (mapping.config && mapping.config[method]) router[way](path, mapping.config[method], mapping[way][method])
+          else router[way](path, mapping[way][method])
+          console.log(path)
         }
-      } else console.log(x + '并没有GET')
-      if ('post' in mapping) {
-        for (let p in mapping.post) {
-          console.log(p, 'pppp', mapping.config)
-          let path = '/' + x.substr(0, x.length - 3) + '/' + p
-          if (mapping.config && mapping.config[p]) router.post(path, mapping.config[p], mapping.post[p])
-          else router.post(path, mapping.post[p])
-        }
-      } else console.log(x + '并没有post')
-    })
-    return router.routes()
-  }
+      }
+    }
+  })
+  return router.routes()
+}
